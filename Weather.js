cb6b7287ec6fb17452b1afb6bfd0044a9c4f4d36
wonBR <76773366+wonBR@users.noreply.github.com>
2021-01-22 14:59:20 +0900

    icon

--- Weather.js
+++ Weather.js
@@ -1,21 +1,46 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Weather({ temp }) {
   return (
-    <View style= {styles.container}>
-      <Text>{temp}</Text>
+    <View style={styles.container}>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons name="weather-fog" size={96} color="black" />
+        <Text style={styles.temp}>{temp}℃</Text>
+      </View>
+      <View style={styles.halfContainer}></View>
     </View>
   );
 }
 
-Weather.PropTypes = {
+Weather.propTypes = {
   temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Dust",
+  ]).isRequired,
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 42,
+  },
+  halfContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
